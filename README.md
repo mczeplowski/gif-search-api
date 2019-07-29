ea36de0af360fe02589d47ff2c287e8764a69f5c
Mateusz Czeplowski <mateusz.czeplowski@kinguin.io>
2019-07-29 17:30:50 +0200

    Added fetching data

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # gif-search-api
 Service that allows to search gifs from two pages by phrase.
+
+# How to run
+
+You have to install nodejs on your computer and get two api keys from services: giphy, pixabay.
+
+Steps:
+
+1. `
+npm install
+`
+
+2. `
+npm run compile
+`
+
+3. `
+npm run server -- --pixabayApiKey=xxxxxxx --giphyApiKey=xxxxxxxx
+`
+
+# How to develop
+
+Steps:
+
+1. `
+npm install
+`
+
+2. You have to create file 'api-keys.json' in main project catalog contains `
+{
+  pixabayApiKey: xxxxx,
+  giphyApiKey: xxxxx,
+}
+`
+
+3. `
+npm start
+`
--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import axios from 'axios';
+import nconf from 'nconf';
+import apiKeys from '../api-keys.json';
+
+nconf.argv().defaults(apiKeys);
+
+const giphyApiKey = nconf.get('giphyApiKey');
+const pixabayApiKey = nconf.get('pixabayApiKey');
 
 const port = 8081;
 const server = express();
@@ -12,5 +20,43 @@ server
   });
 
 async function search(req, res) {
-  res.send({ phrase: req.query.phrase });
+  if (!req.query.phrase) {
+    return res.send({ error: 'Phrase not passed' });
+  }
+
+  const { phrase } = req.query;
+
+  const promises = [
+    axios.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&limit=10&q=${phrase}`,
+    ),
+    axios.get(
+      `https://pixabay.com/api/?key=${pixabayApiKey}&per_page=10&q=${phrase}`,
+    ),
+  ];
+
+  const data = await Promise.all(promises)
+    .then(values => values.map(a => a.data))
+    .then(values => values.map(normalize))
+    .then(values => values.reduce((a, value) => a.concat(value), []));
+
+  return res.send({ phrase, data });
+}
+
+function normalize(array) {
+  if (!array.data && !array.hits) {
+    return [];
+  }
+
+  if (array.data) {
+    return array.data.map(gif => {
+      const { url } = gif.images.downsized_medium;
+      return { url };
+    });
+  }
+
+  return array.hits.map(img => {
+    const { webformatURL: url } = img;
+    return { url };
+  });
 }
